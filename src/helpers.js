9e9dde63be24b17de9c18257d94426c3a4702bd6
JordanRhysSmith <jordanrhysgtr@gmail.com>
2019-07-22 10:12:51 +0100

    removed console logs from currency func

--- src/helpers.js
+++ src/helpers.js
@@ -1,10 +1,7 @@
 const toCurrency = (value) => {
     let str = (value * 100).toString();
-    console.log(str);
     let start = str.substr(0, (str.length - 2));
-    console.log(start);
     let end = str.substr(-2);
-    console.log(end);
     return '£' + start + '.' + end;
 }
 
