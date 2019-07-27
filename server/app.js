584b974266b15d2c13dbcb3e98e909a9feb01da1
JordanRhysSmith <jordanrhysgtr@gmail.com>
2019-07-27 15:27:36 +0100

    moved api keys to external file

--- server/app.js
+++ server/app.js
@@ -2,11 +2,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config();
 
 var productsRouter = require('./routes/products');
 
+const apiKey = process.env.DB_KEY;
+
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://jordan:uHyWLakjZp3TQvbf@cluster0-eyvt0.mongodb.net/VGX?retryWrites=true&w=majority';
+var mongoDB = apiKey;
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB Connection Error:'));
