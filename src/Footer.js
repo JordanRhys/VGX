191f2111b08ba19dbc6f784d3d75878240d4245b
JordanRhysSmith <jordanrhysgtr@gmail.com>
2019-07-28 12:20:32 +0100

    removed logo from footer

--- src/Footer.js
+++ src/Footer.js
@@ -5,11 +5,6 @@ const Footer = () => {
 
     return (
         <footer className='Footer__container'>
-            <img
-                className='Footer__img'
-                src={process.env.PUBLIC_URL + '/VGX.svg'}
-                alt='VGX Logo'
-            />
             <p className='Footer__text'>
                 Video Game eXchange (VGX), is a portfolio project from JordanRhys.dev
             </p>
