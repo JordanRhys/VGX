f01dbc2e3324b367e353894cf4afe435d9860ffe
JordanRhysSmith <jordanrhysgtr@gmail.com>
2019-08-01 08:31:37 +0100

    Removed all files

--- server/models/top_product.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-require('./product');
-
-var TopProductsSchema = new Schema({
-    product: {type: Schema.Types.ObjectId, ref: 'Product'},
-});
-
-module.exports = mongoose.model('TopProducts', TopProductsSchema);
--- src/helpers.js
+++
@@ -1,8 +0,0 @@
-const toCurrency = (value) => {
-    let str = (value * 100).toString();
-    let start = str.substr(0, (str.length - 2));
-    let end = str.substr(-2);
-    return '£' + start + '.' + end;
-}
-
-export { toCurrency };
